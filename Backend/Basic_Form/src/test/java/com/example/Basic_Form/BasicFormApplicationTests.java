a67cc8e8410814da2a4ff76be961ea346ff1589d
YASH KANJIYA <70482112+YashKanjiya@users.noreply.github.com>
2024-04-19 10:00:05 +0530

    second commit

---
+++ Backend/Basic_Form/src/test/java/com/example/Basic_Form/BasicFormApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Basic_Form;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BasicFormApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
---
+++ README.md
@@ -0,0 +1,1 @@
+#Basic form to Create data
